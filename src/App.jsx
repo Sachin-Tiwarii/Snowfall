55b52f1d2acda242daf900023080053690bcc68c
sachin <sachinn.t279@gmail.com>
2026-01-06 13:06:41 +0530

    frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-4xl font-bold text-green-700">
+      CivicSetu
+    </div>
+  )
+}
+
+export default App
